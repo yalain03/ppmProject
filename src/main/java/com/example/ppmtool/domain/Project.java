d07d8ce621365b7d8a342d71ee744b55326c8f75
unknown <yalain9@gmail.com>
2019-06-30 18:32:08 -0500

    New Project Errors

--- src/main/java/com/example/ppmtool/domain/Project.java
+++ src/main/java/com/example/ppmtool/domain/Project.java
@@ -1,6 +1,10 @@
 package com.example.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -9,13 +13,22 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Project name is required")
     private String projectName;
+    @NotBlank(message = "Project identifier is required")
+    @Size(min=4, max=5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+    @NotBlank(message = "Project descritpion is required")
     private String description;
+    @JsonFormat(pattern = "yyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyy-mm-dd")
     private Date end_date;
 
+    @JsonFormat(pattern = "yyy-mm-dd")
     private Date created_At;
+    @JsonFormat(pattern = "yyy-mm-dd")
     private Date updated_At;
 
     public Project() {}

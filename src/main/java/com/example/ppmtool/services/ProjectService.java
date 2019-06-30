0015afe12c266e58b3a870a30f3ee247e1308b99
unknown <yalain9@gmail.com>
2019-06-30 17:38:24 -0500

    Create New Project

---
+++ src/main/java/com/example/ppmtool/services/ProjectService.java
@@ -0,0 +1,18 @@
+package com.example.ppmtool.services;
+
+import com.example.ppmtool.domain.Project;
+import com.example.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project) {
+
+        return projectRepository.save(project);
+    }
+}

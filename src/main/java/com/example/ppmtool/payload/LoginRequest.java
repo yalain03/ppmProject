8dc67e9ba243fca15931f54ed6146889901e6a49
unknown <yalain9@gmail.com>
2019-07-12 16:38:05 -0500

    authentication setup

---
+++ src/main/java/com/example/ppmtool/payload/LoginRequest.java
@@ -0,0 +1,18 @@
+package com.example.ppmtool.payload;
+
+import javax.validation.constraints.NotBlank;
+
+public class LoginRequest {
+    @NotBlank(message = "Please enter your username")
+    private String username;
+    @NotBlank(message = "Please enter your password")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
